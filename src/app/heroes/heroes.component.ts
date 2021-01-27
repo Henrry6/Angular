30521521351ebf2fd1ce89fc62483b829ad9feb5
Henrry Alvarado <princedark393@gmail.com>
2021-01-27 15:17:06 -0500

    Error con app-routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,20 +10,16 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] | null = null;;
-  hero: Hero = { id: 1, name: 'Henrry Alvarado' };
-  selectedHero: Hero | null = null;
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-  
-  ngOnInit(): void {
+  heroes: Hero[] | null = null;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
     this.getHeroes();
   }
-  seleccionar (hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
